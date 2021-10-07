47d8ea671a3a64de7f6f83bc1d163c64ecd7e3b3
2057172 <80460572+2057172@users.noreply.github.com>
2021-10-07 20:46:50 +0200

    Update TagManager.asset

--- SemesterProject/Assets/Scripts/playerMovement.cs
+++ SemesterProject/Assets/Scripts/playerMovement.cs
@@ -20,61 +20,6 @@ public class playerMovement : MonoBehaviour
 
     Rigidbody2D rb;
 
-
-    //FUEL AND HEALTH STATS
-    public health_and_fuel Health_And_Fuel;
-
-    public int maxHealth = 100;
-    public int currentHealth;
-    public int shipDamage = 10;
-
-    public int maxFuel = 100;
-    public int currentFuel;
-    public int fuel = 2;
-
-    void Start()
-    {
-        currentHealth = maxHealth;
-        Health_And_Fuel.setMaxHealth(maxHealth);
-
-        currentFuel = maxFuel;
-        Health_And_Fuel.setMaxFuel(maxFuel);
-    }
-
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            currentFuel -= fuel;
-            Health_And_Fuel.setCurrentFuel(currentFuel);
-
-            if (currentFuel < 0)
-            {
-                currentFuel = 0;
-            }
-        }
-
-    }
-
-    void ShipCollision()
-    {
-        currentHealth -= shipDamage;
-        Health_And_Fuel.setCurrentHealth(currentHealth);
-        if (currentHealth < 0)
-        {
-            currentHealth = 0;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Collision")
-        {
-            ShipCollision();
-        }
-    }
-    /////
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
