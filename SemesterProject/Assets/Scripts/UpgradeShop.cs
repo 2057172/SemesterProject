60fd2d9c4eb33e6126eda927a3d7eced667318a1
Cheyenne-Peter <2180052@students.wits.ac.za>
2021-10-22 13:58:16 +0200

    More Upgrade Things

--- SemesterProject/Assets/Scripts/UpgradeShop.cs
+++ SemesterProject/Assets/Scripts/UpgradeShop.cs
@@ -7,8 +7,16 @@ public class UpgradeShop : MonoBehaviour
     public GameObject repairPanel;
     public bool isAtShop;
     public bool shopOP;
-   
     public Text instruction;
+    public GameManager gameMan;
+
+
+    /// <summary>
+    /// All of the variables for the once off upgrades
+    /// </summary>
+    public int firePro;
+    public int icePro;
+    public int radPro;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +27,7 @@ public class UpgradeShop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /// when the rocket is in the vacinity of the mechanic, this is to open and close the panel with the options
         if (isAtShop)
         {
             if (Input.GetKeyDown(KeyCode.E) && !shopOP)
@@ -36,7 +45,32 @@ public class UpgradeShop : MonoBehaviour
                 Debug.Log("CLOSE");
             }
         }
-       
+
+        ///Upgrade things
+        
+        // if the money is equal or less than 0, then it stays at 0
+
+        if (gameMan.money <= 0)
+        {
+            gameMan.money = 0;
+        }
+
+        // if you don't have enough money for a upgrade, you cannot purchase it
+
+        if (gameMan.money < firePro)
+        {
+            //can't purchase fire protection
+        }
+
+        if (gameMan.money < icePro)
+        {
+            //can't purchase ice protection
+        }
+
+        if (gameMan.money < radPro)
+        {
+            //can't purchase radiation protection
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -55,5 +89,20 @@ public class UpgradeShop : MonoBehaviour
             repairPanel.SetActive(false);
             instruction.text = null; 
         }
+    }
+
+    public void Upgrade1()
+    {
+        gameMan.money -= firePro;
+    }
+
+    public void Upgrade2()
+    {
+        gameMan.money -= icePro;
+    }
+
+    public void Upgrade3()
+    {
+        gameMan.money -= radPro;
     }
 }
