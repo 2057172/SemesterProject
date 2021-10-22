9269fbffebf9db72a584212c6eb41225be9b49ea
2057172 <80460572+2057172@users.noreply.github.com>
2021-10-22 11:30:56 +0200

    OrderTaking

---
+++ SemesterProject/Assets/Scripts/OrderManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderManager : MonoBehaviour
+{
+    public WindowQuestPointer pointer;
+
+    public bool currentlyHasOrder;
+    public bool BurgerAndFriesB, IceCreamB, SandwichB;
+
+    public Transform pickUpZone;
+    public Transform BAndFriesDestination;
+
+    public Transform BurgerAndFriedDropoff;
+
+    public Image Menu;
+    public float totalCommision;
+
+    public GameManager gm;
+    public Text OrderMoneyTXT;
+
+    void Start()
+    {
+        currentlyHasOrder = false;
+        OrderMoneyTXT.gameObject.SetActive(false);
+        BurgerAndFriesB = false;
+        IceCreamB = false;
+        SandwichB = false;       
+    }
+
+
+    public void BurgerAndFries()
+    {
+        if (gm.money >= 20)
+        {
+            gm.money = gm.money - 20;
+            currentlyHasOrder = true;
+            BurgerAndFriesB = true;
+            pointer.targetPosition = BurgerAndFriedDropoff.position;
+            Menu.gameObject.SetActive(false);
+            totalCommision = 20 + (Vector3.Distance(pickUpZone.position, BAndFriesDestination.position) * 0.8f);
+            OrderMoneyTXT.text = "Total: $" + Mathf.Round(totalCommision);
+            OrderMoneyTXT.gameObject.SetActive(true);
+        }
+        else { Debug.Log("Can't Afford"); }
+    }
+}
