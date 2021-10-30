0e8ea59dfefed645fdfc687a1a9fa98f95b971e8
2057172 <80460572+2057172@users.noreply.github.com>
2021-10-30 13:11:06 +0200

    MapPan

---
+++ SemesterProject/Assets/Scripts/MapCameraMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    public Camera cam;
+
+    private Vector3 dragOrigin;
+
+    void Update()
+    {
+        panCam();
+    }
+    private void panCam()
+    { 
+        if (Input.GetMouseButtonDown(0))
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            print("origin " + dragOrigin + " new position " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference" + difference);
+
+            cam.transform.position += difference / 2;
+        }
+    }
+}
