329303d0d69b1719a3f2e3fb8ae98f1504376fdd
DiyankaGovender <2104082@students.wits.ac.za>
2021-11-24 00:17:07 +0200

    Phone Order Food Finalized

--- SemesterProject/Assets/Scripts/Dee New Scripts/menu_Items.cs
+++ SemesterProject/Assets/Scripts/Dee New Scripts/menu_Items.cs
@@ -47,6 +47,7 @@ public class menu_Items : MonoBehaviour
     public Sprite riceImg;
     public Sprite donutsImg;
     public Sprite iceCreamImg;
+    public Sprite cupcakeImg;
     public Sprite waffleImg;
     public Sprite milkshakeImg;
     public Sprite waterImg;
@@ -579,6 +580,14 @@ public class menu_Items : MonoBehaviour
 
             case "Doughnuts":
                 return donutsImg;
+                break;
+
+            case "Waffles":
+                return waffleImg;
+                break;
+
+            case "Cupcake":
+                return cupcakeImg;
                 break;
 
             case "Ice Cream":
