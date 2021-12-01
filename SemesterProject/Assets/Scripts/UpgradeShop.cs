f722e15175ac24f4d0ed5b06c1057a680c76a53a
2057172 <80460572+2057172@users.noreply.github.com>
2021-12-01 17:14:26 +0200

    Revert "Merge branch 'main' into Cam"
    This reverts commit 7c8c6959ffc5affb71969eafe18e64486439b5e1, reversing
    changes made to 02f592859ce66bf1c7348256ea1a4a54fb4c5f8a.

--- SemesterProject/Assets/Scripts/UpgradeShop.cs
+++ SemesterProject/Assets/Scripts/UpgradeShop.cs
@@ -18,6 +18,7 @@ public class UpgradeShop : MonoBehaviour
     public int firePro;
     public int radPro;
     public int hardcorePro;
+
     
     [SerializeField] public bool icePurchased;
     [SerializeField] public bool firePurchased;
@@ -80,7 +81,6 @@ public class UpgradeShop : MonoBehaviour
     /// </summary>
     /// 
 
-    public int maxOrderCapacity = 2; 
 
     public int carryCapUp1Cost;
     public int carryCapUp2Cost;
@@ -100,20 +100,37 @@ public class UpgradeShop : MonoBehaviour
 
     public Button repairButton;
 
+    public TextMeshProUGUI priceTXT;
+
 
   
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
 
-}
+        heatButton.GetComponentInChildren<TextMeshProUGUI>().text = "Heat Protection: " + "$" + firePro;
+        iceButton.GetComponentInChildren<TextMeshProUGUI>().text = "Ice Protection: " + "$" + icePro;
+        radButton.GetComponentInChildren<TextMeshProUGUI>().text = "Radiation Protection: " + "$" + radPro;
+        hardcoreButton.GetComponentInChildren<TextMeshProUGUI>().text = "Hardcore Proection: " + "$" + hardcorePro;
 
-    
-    
+        healthUp1Button.GetComponentInChildren<TextMeshProUGUI>().text = "Health Upgrade 1: " + "$" + HealthUp1Cost;
+        healthUp2Button.GetComponentInChildren<TextMeshProUGUI>().text = "Health Upgrade 2: " + "$" + HealthUp2Cost;
+        healthUp3Button.GetComponentInChildren<TextMeshProUGUI>().text = "Health Upgrade 3: " + "$" + HealthUp3Cost;
 
+        fuelUp1Button.GetComponentInChildren<TextMeshProUGUI>().text = "Fuel Upgrade 1: " + "$" + FuelUp1Cost;
+        fuelUp2Button.GetComponentInChildren<TextMeshProUGUI>().text = "Fuel Upgrade 2: " + "$" + FuelUp2Cost;
+        fuelUp3Button.GetComponentInChildren<TextMeshProUGUI>().text = "Fuel Upgrade 3: " + "$" + FuelUp3Cost;
+
+        carryCapUp1Button.GetComponentInChildren<TextMeshProUGUI>().text = "Order Capacity Upgrade 1: " + "$" + carryCapUp1Cost;
+        carryCapUp2Button.GetComponentInChildren<TextMeshProUGUI>().text = "Order Capacity Upgrade 2: " + "$" + carryCapUp2Cost;
+        carryCapUp3Button.GetComponentInChildren<TextMeshProUGUI>().text = "Order Capacity Upgrade 3: " + "$" + carryCapUp3Cost;
+
+
+        repairButton.GetComponentInChildren<TextMeshProUGUI>().text = "Repair Ship: " + "$" + repairScript.repairCost;
+    }
+
+    // Update is called once per frame
     void Update()
     {
         heatButton.GetComponentInChildren<TextMeshProUGUI>().text = "Heat Protection: " + "$" + firePro;
@@ -234,7 +251,6 @@ public class UpgradeShop : MonoBehaviour
         if (gameMan.money < HealthUp1Cost || healthUP1purchased)     // If you don't have enough money for Health Upgrade 1 or have purchased it
         {
             healthUp1Button.interactable = false;
-            //healthUp1Button.enabled = false;
            
         }
         else
@@ -288,6 +304,7 @@ public class UpgradeShop : MonoBehaviour
     }
 
 
+   
 
     public void UpgradeHeat()
     {
@@ -304,16 +321,6 @@ public class UpgradeShop : MonoBehaviour
         {
             firePurchased = false;
         }
-
-        if (gameMan.money < firePro || firePurchased)
-        {
-            //can't purchase fire protection
-            heatButton.interactable = false;
-        }
-        else
-        {
-            heatButton.interactable = true;
-        }
     }
 
     public void UpgradeIce()
@@ -326,18 +333,7 @@ public class UpgradeShop : MonoBehaviour
         else
         {
             icePurchased = false;
-        }
-
-        if (gameMan.money < icePro || icePurchased)
-        {
-            //can't purchase ice protection
-            iceButton.interactable = false;
-        }
-        else
-        {
-            iceButton.interactable = true;
-        }
-
+        } 
     }
 
     public void UpgradeRadiation()
@@ -351,17 +347,6 @@ public class UpgradeShop : MonoBehaviour
         else
         {
             radPurchased = false;
-        }
-
-
-        if (gameMan.money < radPro || radPurchased)
-        {
-            //can't purchase radiation protection
-            radButton.interactable = false;
-        }
-        else
-        {
-            radButton.interactable = true;
         }
     }
 
@@ -378,16 +363,6 @@ public class UpgradeShop : MonoBehaviour
         {
             hardcorePurchased = false;
         }
-
-        if (gameMan.money < hardcorePro || hardcorePurchased)
-        {
-            //can't purchase radiation protection
-            hardcoreButton.interactable = false;
-        }
-        else
-        {
-            hardcoreButton.interactable = true;
-        }
     }
 
     public void FuelUpgrade1()
@@ -396,27 +371,13 @@ public class UpgradeShop : MonoBehaviour
         {
             gameMan.money -= FuelUp1Cost;
             playerMov.maxFuel = maxFuelUpgrade1;
-            playerMov.currentFuel = maxFuelUpgrade1;
             fuelUP1purchased = true;
-
-            playerMov.fuel_text.text = playerMov.currentFuel.ToString() + "/" + playerMov.maxFuel.ToString();
-            playerMov.health_text.text = playerMov.currentHealth.ToString() + "/" + playerMov.maxHealth.ToString();
         }
         else
         {
             fuelUP1purchased = false;
         }
-
-
-        if (gameMan.money < FuelUp1Cost || fuelUP1purchased)     // If you don't have enough money for Fuel Upgrade 1 or have purchased it
-        {
-            fuelUp1Button.interactable = false;
-        }
-        else
-        {
-            fuelUp1Button.interactable = true;
-        }
-
+       
     }
 
     public void FuelUpgrade2()
@@ -425,21 +386,11 @@ public class UpgradeShop : MonoBehaviour
         {
             gameMan.money -= FuelUp2Cost;
             playerMov.maxFuel = maxFuelUpgrade2;
-            playerMov.currentFuel = maxFuelUpgrade2;
             fuelUP2purchased = true;
         }
         else
         {
             fuelUP2purchased = false;
-        }
-
-        if (gameMan.money < FuelUp2Cost || fuelUP2purchased)     // If you don't have enough money for Fuel Upgrade 2 or have purchased it
-        {
-            fuelUp2Button.interactable = false;
-        }
-        else
-        {
-            fuelUp2Button.interactable = true;
         }
     }
 
@@ -450,20 +401,10 @@ public class UpgradeShop : MonoBehaviour
             gameMan.money -= FuelUp3Cost;
             playerMov.maxFuel = maxFuelUpgrade3;
             fuelUP3purchased = true;
-            playerMov.currentFuel = maxFuelUpgrade3;
         }
         else
         {
             fuelUP3purchased = false;
-        }
-
-        if (gameMan.money < FuelUp3Cost || fuelUP3purchased)     // If you don't have enough money for Fuel Upgrade 3 or have purchased it
-        {
-            fuelUp3Button.interactable = false;
-        }
-        else
-        {
-            fuelUp3Button.interactable = true;
         }
     }
 
@@ -472,23 +413,12 @@ public class UpgradeShop : MonoBehaviour
         if (gameMan.money >= HealthUp1Cost)
         {
             gameMan.money -= HealthUp1Cost;
-            playerMov.currentHealth = maxFuelUpgrade1;
             playerMov.maxHealth = maxHealthUpgrade1;
             healthUP1purchased = true;
-
-            healthUp1Button.interactable = false;
-            //healthUp1Button.enabled = false;
         }
         else
         {
             healthUP1purchased = false;
-        }
-
-        if (gameMan.money < HealthUp1Cost || healthUP1purchased)     // If you don't have enough money for Health Upgrade 1 or have purchased it
-        {
-            healthUp1Button.interactable = false;
-            //healthUp1Button.enabled = false;
-
         }
     }
 
@@ -497,7 +427,6 @@ public class UpgradeShop : MonoBehaviour
         if (gameMan.money >= HealthUp2Cost && healthUP1purchased)
         {
             gameMan.money -= HealthUp2Cost;
-            playerMov.currentHealth = maxFuelUpgrade2;
             playerMov.maxHealth = maxHealthUpgrade2;
             healthUP2purchased = true;
         }
@@ -505,16 +434,6 @@ public class UpgradeShop : MonoBehaviour
         {
             healthUP2purchased = false;
         }
-
-        if (gameMan.money < HealthUp2Cost || healthUP2purchased)     // If you don't have enough money for Health Upgrade 2 or have purchased it
-        {
-            healthUp2Button.interactable = false;
-        }
-        else
-        {
-            healthUp2Button.interactable = true;
-        }
-
     }
 
     public void HealthUpgrade3()
@@ -522,7 +441,6 @@ public class UpgradeShop : MonoBehaviour
         if (gameMan.money >= HealthUp3Cost && healthUP2purchased)
         {
             gameMan.money -= HealthUp3Cost;
-            playerMov.currentHealth = maxFuelUpgrade3;
             playerMov.maxHealth = maxHealthUpgrade3;
             healthUP3purchased = true;
         }
@@ -530,16 +448,6 @@ public class UpgradeShop : MonoBehaviour
         {
             healthUP3purchased = false;
         }
-
-        if (gameMan.money < HealthUp3Cost || healthUP3purchased)     // If you don't have enough money for Health Upgrade 3 or have purchased it
-        {
-            healthUp3Button.interactable = false;
-        }
-        else
-        {
-            healthUp3Button.interactable = true;
-        }
-
     }
 
     public void CarryCapUpgrade1()
@@ -549,21 +457,11 @@ public class UpgradeShop : MonoBehaviour
             gameMan.money -= carryCapUp1Cost;
             carryCapUP1purchased = true;
 
-            maxOrderCapacity = 3;
+            //DO SOMETHING
         }
         else
         {
             carryCapUP1purchased = false;
-        }
-
-        // Order Carry Capacity Things
-        if (gameMan.money < carryCapUp1Cost || carryCapUP1purchased)
-        {
-            carryCapUp1Button.interactable = false;
-        }
-        else
-        {
-            carryCapUp1Button.interactable = true;
         }
     }
 
@@ -575,21 +473,11 @@ public class UpgradeShop : MonoBehaviour
             gameMan.money -= carryCapUp2Cost;
             carryCapUP2purchased = true;
 
-            maxOrderCapacity = 4;
+            //DO SOMETHING
         }
         else
         {
             carryCapUP2purchased = false;
-        }
-
-        // Order Carry Capacity Things
-        if (gameMan.money < carryCapUp2Cost || carryCapUP2purchased)
-        {
-            carryCapUp2Button.interactable = false;
-        }
-        else
-        {
-            carryCapUp2Button.interactable = true;
         }
     }
 
@@ -601,21 +489,11 @@ public class UpgradeShop : MonoBehaviour
             gameMan.money -= carryCapUp3Cost;
             carryCapUP3purchased = true;
 
-            maxOrderCapacity = 5;
+            //DO SOMETHING
         }
         else
         {
             carryCapUP3purchased = false;
-        }
-
-        // Order Carry Capacity Things
-        if (gameMan.money < carryCapUp3Cost || carryCapUP3purchased)
-        {
-            carryCapUp3Button.interactable = false;
-        }
-        else
-        {
-            carryCapUp3Button.interactable = true;
         }
     }
 }
